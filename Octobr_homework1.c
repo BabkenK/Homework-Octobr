8ee6e3a233eec10e9f3033499a5fc12703ec24ed
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-10-05 21:04:08 +0400

    Հայտարարեք ցանկացած երեք տարբերվող տիպի ցուցիչներ: Արժեքավորեք ցուցիչները և տպեք էկրանին տվյալ ցուցիչների մեջ պահված հասցեները։
    Հայտարարեք ցանկացած երեք տարբերվող տիպի ցուցիչներ: Արժեքավորեք ցուցիչները և տպեք էկրանին տվյալ ցուցիչների մեջ պահված հասցեները։

---
+++ Octobr_homework1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    char y;
+    double z;
+    
+    printf("%p \n", &x);
+    printf("%p \n", &y);
+    printf("%p \n", &z);
+    
+    return 0;
+}
